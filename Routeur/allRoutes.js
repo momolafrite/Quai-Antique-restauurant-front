b166f292e1826f21d32142ba793c463b57bfbe92
Morgan Mesuron <mmesuron@gmail.com>
2025-05-22 17:18:15 +0200

    install bootstrap

--- Routeur/allRoutes.js
+++ Routeur/allRoutes.js
@@ -1,6 +1,9 @@
 import Route from "./route.js";
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
+];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
